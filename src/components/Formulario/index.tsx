8563495af2dc6c9fa402993f88e3595f93905f3e
Artur Rabello <artur_rabello@yahoo.com.br>
2024-10-30 19:36:27 -0300

    commit 2

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
+import { Itarefa } from '../../types/Itarefa';
+import {v4 as uuidv4} from 'uuid';
 
-class Formulario extends React.Component {
+
+class Formulario extends React.Component<{
+  setTarefas:React.Dispatch<React.SetStateAction<Itarefa[]>>
+}>{
   state ={
     tarefa: "",
     tempo: "00:00"
@@ -10,6 +15,18 @@ class Formulario extends React.Component {
 
   adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
     evento.preventDefault();
+    this.props.setTarefas(tarefasAntigas => [
+      ...tarefasAntigas,
+      {...this.state, selecionado: false,
+         completado: false,
+         id: uuidv4()
+        }
+     ]
+    );
+    this.setState({
+      tarefa: "",
+      tempo: "00:00"
+    })
   }
 
   render() {
@@ -24,7 +41,7 @@ class Formulario extends React.Component {
             name="tarefa"
             id="tarefa"
             value={this.state.tarefa}
-            onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
+            onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
             placeholder="O que você quer estudar"
             required
           />
