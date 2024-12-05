eaf32eb2a4f1bbce245750c430faeeddb6ebff69
Artur Rabello <artur_rabello@yahoo.com.br>
2024-12-05 15:43:26 -0300

    Finalizado

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,15 +1,42 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../common/utils/time";
+import { Itarefa } from "../../types/Itarefa";
+import { useEffect, useState } from "react";
+import { time } from "console";
 
-export default function Cronometro() {
+interface props{
+  selecionado: Itarefa | undefined
+  finalizarTarefa: () => void
+}
+
+export default function Cronometro({selecionado,
+  finalizarTarefa}: props) {
+  const [tempo, setTempo] = useState<number>();
+  useEffect (() => {
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+    finalizarTarefa();
+  }, [selecionado]);
+  function regressiva(contador: number = 0){
+    setTimeout(() => {
+      if(contador > 0){
+        setTempo(contador - 1);
+        return regressiva(contador - 1)
+      }
+      finalizarTarefa();
+    }, 1000);
+  }
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo}/>
       </div>
-      <Botao>
+      <Botao onClick={() => regressiva(tempo)}>
         Começar!
       </Botao>
     </div>
